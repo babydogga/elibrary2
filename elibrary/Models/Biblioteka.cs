b139321095545e50e59cdca5f0dcc4b9b7c58cef
Sylwester Szwedzki <sylwester.szwedzki@microsoft.wsei.edu.pl>
2025-03-11 21:51:33 +0100

    Create button dla bibilioteka

--- elibrary/Models/Biblioteka.cs
+++ elibrary/Models/Biblioteka.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace elibrary.Models
 {
@@ -14,6 +15,7 @@ namespace elibrary.Models
         public string DescBib { get; set; }
 
         //relacje
+        [ValidateNever]
         public List<Ksiazka> Ksiazki { get; set; }
     }
 }
